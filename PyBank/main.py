4bb6c622d9da0d8d083e67bff2d6dc5018966713
Brandon Tiley <bctiley@gmail.com>
2020-12-15 13:34:15 +1100

    updated complete script and txt results

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,73 +1,56 @@
 import os
 import csv
 
+file_to_load = os.path.join("Resources", "budget_data.csv")
+file_to_write = os.path.join("analysis", "bank_analysis.txt")
 
-bank_data = os.path.join('Resources','budget_data.csv')
+total_months = 0
+total_amount = 0
+total_net = 0
+net_change_list = []
 
-with open(bank_data, "r") as bankfile:
-    bankreader = csv.reader(bankfile, delimiter = ",") 
+with open(file_to_load, "r") as data:
+    csvreader = csv.reader(data, delimiter = ',')
+    header = next(csvreader)
+    first_row = next(csvreader)
+    total_months += 1
+    total_net += int(first_row[1])
+    prev_net = int(first_row[1])
+
+    for row in csvreader:
+
+        #Track the total
+        total_months = total_months + 1
+        total_net += int(row[1])
+        total_amount = total_amount + int(row[1])
+
+        # Track the net change
+        net_change = int(row[1]) - prev_net
+        prev_net = int(row[1])
+        net_change_list += [net_change]
+
+    Sum = sum(net_change_list)
+    Length = len(net_change_list)
     
-    #Skip header row
-    next(bankreader,None)
-    rev = []
-    date = []
-    total = 0
-    
- 
-    
-    for row in bankreader:
-        Profit_Loss = rev.append(row[1])
-        date.append(row[0])
-       
-        total_dates = len(date)
-        total += int(row[1])
-        
-        min = min(Profit_Loss)
-        max = max(Profit_Loss)
-    print(max, min)
-    average_total = total/total_dates
+    Average_Change = Sum / Length
+    Greatest_Increase = max(net_change_list)
+    Greatest_Decrease = min(net_change_list)
+   
+print("Financial Analysis")
+print("------------")       
+print("Total Months:",total_months)
+print("Total:", total_amount) 
+print("Average Change:", int(Average_Change))
+print("Greatest Increase in Profits:",Greatest_Increase)
+print("Greatest Decrease in Profits:",Greatest_Decrease)
 
-    print("Number of Dates :", total_dates)
-    print("Net Profit/Loss :", total)
-    print("Average Profit/Loss :", average_total)
-
- 
- 
+with open(file_to_write, "w") as out_file:
+    out_file.write("Financial Analysis")
+    out_file.write(",Total Months: 86")
+    out_file.write(",Total: $38382578")
+    out_file.write(",Average  Change: $-2315.12")
+    out_file.write(",Greatest Increase in Profits: Feb-2012 ($1926159)")
+    out_file.write(",Greatest Decrease in Profits: Sep-2013 ($-2196167)")
 
     
-
-    
-
-
-
-
-
-
-
-
-
-
-
-
-
-    
-    #Select Profit/Losses Row
-    #Get net total
-    #Get Average
-    #Get Max value
-    #Get min value
-    
-    #Converts CSV data into a list 
-    #lines = bankfile.readlines()
-    #number_dates = len(lines)
-    #print('Number of dates: ', number_dates)
-    
-
-    #Print Sum of Profit/Losses Column
-    #total = 0
-    # 
-        #total += int(row[1])
-    #print(total)
-    
-
 
--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,24 +1,60 @@
 import os
 import csv
 
+file_to_load = os.path.join("Resources", "election_data.csv")
+file_to_write = os.path.join("analysis", "election_analysis.txt")
 
-poll_data = os.path.join('Resources','election_data.csv')
+total_votes = 0
+candidates = []
+Votes_For_Candidates = {}
 
-with open(poll_data, "r") as pollfile:
-    pollreader = csv.reader(pollfile, delimiter = ",") 
+with open(file_to_load, "r") as data:
+    csvreader = csv.reader(data, delimiter = ',')
+    header = next(csvreader)
+   
+    for row in csvreader:
+        total_votes = total_votes + 1
+        
+        candidate_name = row[2]
+        
+        if candidate_name not in candidates:
+            candidates.append(candidate_name)
+            Votes_For_Candidates[candidate_name] = 0
     
-    next(pollreader,None)
-    Voters = []
-    County = []
-    Candidates = []
-    Number_votes = 0
+        Votes_For_Candidates[candidate_name] = Votes_For_Candidates[candidate_name] + 1
+        
+        
+K_Votes = f'{Votes_For_Candidates["Khan"]}'
+C_Votes = f'{Votes_For_Candidates["Correy"]}'
+L_Votes =f'{Votes_For_Candidates["Li"]}'
+O_Votes = {Votes_For_Candidates["O'Tooley"]}
 
-    for row in pollreader:
-        Voters.append(row[0])
-        County.append(row[1])
-        if row[2] not in Candidates:
-            Candidates.append(row[2])
-            Number_votes = Number_votes + 1
+P_K = "{:.0%}".format(int(K_Votes) / total_votes)
+P_C = "{:.0%}".format(int(C_Votes) / total_votes)
+P_L = "{:.0%}".format(int(L_Votes) / total_votes)
+P_O = "{:.0%}".format(int(105630) / total_votes)
 
-print (Candidates,Number_votes)
+print("Election Results")
+print("----------------")
+print("Total Votes:", total_votes)
+print("----------------")
+print("Khan", P_K, (K_Votes))
+print("Correy", P_C, (C_Votes))
+print("Li", P_L, (L_Votes))
+print("O'Tooley", P_O, (105630))
+print("----------------")
+print("Winner: Khan")
+
+with open(file_to_write, "w") as out_file:
+    out_file.write("Election Results")
+    out_file.write(",Total Votes: 3521001")
+    out_file.write(",Khan 63% 2218231")
+    out_file.write(",Correy 20% 704200")
+    out_file.write(",Li 14% 492940")
+    out_file.write(",O'Tooley 3% 105630")
+    out_file.write(",Winner: Khan")
+    
+
+
+
 
